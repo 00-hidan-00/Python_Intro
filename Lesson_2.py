d150a11b352c20d3909b6686613b40565a7477b1
david <david.taranov@gmail.com>
2022-03-20 17:58:53 +0100

    add lesson 2

---
+++ Lesson_2.py
@@ -0,0 +1,102 @@
+# переменная :
+
+# value = 3
+# new_value = 5.1
+# my_str = "Hello!!!"
+
+# print(value, new_value, my_str, print)
+
+# type - функция получения типа объектов
+
+# int - тип целых чисел <class 'int'>
+
+# print(type(value))
+#################################################
+
+# int
+# val_1 = 21
+# val_2 = 4
+# # result = val_1 + val_2
+# # result = val_1 * val_2
+# # result = val_1 - val_2
+# # result = val_1 / val_2
+# # result = val_1 // val_2  # целочисленное деление есть, но не рекомендуется  ( целая часть числа в МАТЕМАТИЧЕСКОМ СМЫСЛЕ)
+# # result = val_1 % val_2 # остаток от деления есть, но не рекомендуется
+# result = val_1 ** val_2 #возведение в степерь
+# # взять в квадрат ( умножить 0.5)
+# result = val_1 ** val_2
+# print(result, type (result))
+# #################################################
+# val_1 = 23.2
+# val_2 = 10.4
+# result = val_1 + val_2
+# result = val_1 * val_2
+# result = val_1 - val_2
+# result = val_1 / val_2
+# result = val_1 // val_2  # целочисленное деление ( целая часть числа в МАТЕМАТИЧЕСКОМ СМЫСЛЕ)
+# result = val_1 % val_2 # остаток от деления
+# print(result, type (result))
+
+#################################################
+
+# value_int = 3
+# value_float = 4.3
+# value_int = value_float
+# value_float = 5
+#
+# # id() - функция получения id объекта
+#
+# print(id(value_int), id(value_float))
+
+#################################################
+
+# a, b = 2, 4
+# a, b = b, a
+# print(a)
+
+#################################################
+
+# my_str = "New_string"
+# my_str = 'New_string'
+# my_str = """New_string"""
+# my_str = '''New_string'''
+# my_str = "FC 'Arsenal'"
+
+# print(my_str, type(my_str))
+
+# new_str = 'My\tname\tis\nvova'
+# new_str = r'My\tname\tis\nvova' # raw string
+
+# f_str = f'My value is {value}'
+
+# f_str = f"{value=}"
+#
+# print(f_str)
+
+
+# my_int = 5
+# my_float = 3.14
+# my_str = "New_string"
+# new_str = '!!!'
+#
+#
+# result = my_str + new_str
+#
+# print(result)
+
+# folder = "images"
+# filename = "photo_1"
+# ext = "png"
+# # path = folder + "/" + filename
+# path = f"{folder}/{filename}.{ext}"
+# print(path)
+
+# PEP8 formatting: Ctrl + Alt + l   !!!!!!!!!!
+
+
+folder = "images"
+filename = "photo_1"
+ext = "png"
+# path = folder + "/" + filename
+path = f"{folder}/{filename}.{ext}"
+print(path)
