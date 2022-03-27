17a192f016a5fc70709dc451a23295642a9917df
david <david.taranov@gmail.com>
2022-03-27 03:25:42 +0200

    add ###Homework_Lesson_4

--- Homework_Lesson_4.py
+++ Homework_Lesson_4.py
@@ -59,75 +59,75 @@
 
 
 
-# В самом низу есро вопрос
-go_loop = True
-
-while go_loop:
-    input_case = input("Выбери тип операции:\n1 +\n2 -\n3 *\n4 /\n")
-
-    if input_case == '1':
-        value_1 = input("Введи первое число:\n")
-        value_2 = input("Введи второе число:\n")
-        try:
-            float_value_1 = float(value_1)
-            float_value_2 = float(value_2)
-            result = float_value_1 + float_value_2
-            print("Ответ: ", result)
-        except ValueError:
-            print('Это не похоже на цирфы :(\nПридётся начать с начала...')
-
-    elif input_case == '2':
-        value_1 = input("Введи первое число:\n")
-        value_2 = input("Введи второе число:\n")
-        try:
-            float_value_1 = float(value_1)
-            float_value_2 = float(value_2)
-            result = float_value_1 - float_value_2
-            print("Ответ: ", result)
-        except ValueError:
-            print('Это не похоже на цирфы :(\nПридётся начать с начала...')
-
-    elif input_case == '3':
-        value_1 = input("Введи первое число:\n")
-        value_2 = input("Введи второе число:\n")
-        try:
-            float_value_1 = float(value_1)
-            float_value_2 = float(value_2)
-            result = float_value_1 * float_value_2
-            print("Ответ: ", result)
-        except ValueError:
-            print('Это не похоже на цирфы :(\nПридётся начать с начала...')
-
-    elif input_case == '4':
-        value_1 = input("Введи первое число:\n")
-        value_2 = input("Введи второе число (ТОЛЬКО НЕ НОЛЬ!)\n")
-        try:
-            float_value_1 = float(value_1)
-            float_value_2 = float(value_2)
-            result = float_value_1 / float_value_2
-            print("Ответ: ", result)
-        except ValueError:
-            print('Это не похоже на цирфы :(\nПридётся начать с начала...')
-        except ZeroDivisionError:
-            print('Ну я же просил...\nДелить на ноль нельзя!' )
-    elif input_case != ('1', '2', '3', '4'):
-        print("Пожалуйста, выберете ОДИН из четырех вариантов !\nПридётся начать с начала...")
-    else:
-        print("Как ты это сделал ???...")
-
-
-
-    continue_request_loop = True
-    while continue_request_loop:
-        continue_request = input('\nЧто бы продолжить введите "y"\nЧто бы прекратить введите "n"\n')
-        if continue_request == "n":
-            # Объясните пожалуйста !
-            # Как тут можно задать больше значений ? Например ("n", "N", "No"),
-            # <<<if continue_request == ("n", "N", "Not")>>> не рабоатет, хотя тут  <<<elif continue_request != ('n', 'y')>>> все идет
-            # Заранее спасибо за ответ !
-            continue_request_loop = False
-            go_loop = False
-        elif continue_request == "y":
-            continue_request_loop = False
-        elif continue_request != ('n', 'y'):
-            print("Следуйте инструкции !")
+# # В самом низу есро вопрос
+# go_loop = True
+#
+# while go_loop:
+#     input_case = input("Выбери тип операции:\n1 +\n2 -\n3 *\n4 /\n")
+#
+#     if input_case == '1':
+#         value_1 = input("Введи первое число:\n")
+#         value_2 = input("Введи второе число:\n")
+#         try:
+#             float_value_1 = float(value_1)
+#             float_value_2 = float(value_2)
+#             result = float_value_1 + float_value_2
+#             print("Ответ: ", result)
+#         except ValueError:
+#             print('Это не похоже на цирфы :(\nПридётся начать с начала...')
+#
+#     elif input_case == '2':
+#         value_1 = input("Введи первое число:\n")
+#         value_2 = input("Введи второе число:\n")
+#         try:
+#             float_value_1 = float(value_1)
+#             float_value_2 = float(value_2)
+#             result = float_value_1 - float_value_2
+#             print("Ответ: ", result)
+#         except ValueError:
+#             print('Это не похоже на цирфы :(\nПридётся начать с начала...')
+#
+#     elif input_case == '3':
+#         value_1 = input("Введи первое число:\n")
+#         value_2 = input("Введи второе число:\n")
+#         try:
+#             float_value_1 = float(value_1)
+#             float_value_2 = float(value_2)
+#             result = float_value_1 * float_value_2
+#             print("Ответ: ", result)
+#         except ValueError:
+#             print('Это не похоже на цирфы :(\nПридётся начать с начала...')
+#
+#     elif input_case == '4':
+#         value_1 = input("Введи первое число:\n")
+#         value_2 = input("Введи второе число (ТОЛЬКО НЕ НОЛЬ!)\n")
+#         try:
+#             float_value_1 = float(value_1)
+#             float_value_2 = float(value_2)
+#             result = float_value_1 / float_value_2
+#             print("Ответ: ", result)
+#         except ValueError:
+#             print('Это не похоже на цирфы :(\nПридётся начать с начала...')
+#         except ZeroDivisionError:
+#             print('Ну я же просил...\nДелить на ноль нельзя!' )
+#     elif input_case != ('1', '2', '3', '4'):
+#         print("Пожалуйста, выберете ОДИН из четырех вариантов !\nПридётся начать с начала...")
+#     else:
+#         print("Как ты это сделал ???...")
+#
+#
+#
+#     continue_request_loop = True
+#     while continue_request_loop:
+#         continue_request = input('\nЧто бы продолжить введите "y"\nЧто бы прекратить введите "n"\n')
+#         if continue_request == "n":
+#             # Объясните пожалуйста !
+#             # Как тут можно задать больше значений ? Например ("n", "N", "No"),
+#             # <<<if continue_request == ("n", "N", "Not")>>> не рабоатет, хотя тут  <<<elif continue_request != ('n', 'y')>>> все идет
+#             # Заранее спасибо за ответ !
+#             continue_request_loop = False
+#             go_loop = False
+#         elif continue_request == "y":
+#             continue_request_loop = False
+#         elif continue_request != ('n', 'y'):
+#             print("Следуйте инструкции !")
