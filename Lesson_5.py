ddd88425e7d2de5daa18e922bf2f988e6c78656a
david <david.taranov@gmail.com>
2022-04-02 22:53:02 +0200

    add Homework_Lesson_5.py

--- Lesson_5.py
+++ Lesson_5.py
@@ -7,80 +7,99 @@
 
 # for
 
-# my_str = "qwerty123456"
-
+my_str = "qwerty123456"
+#
 # for symbol in my_str:
 #     print(symbol)
-
-# for symbol in my_str[:6]:
-#     print(symbol)
-
+#
+for symbol in my_str[:6]:
+    print(symbol)
+#
 # for index in range(5):
 #     print(index)
-
+#
 # for index in range(len(my_str)):
 #     print(index, my_str[index])
-
+#
 # for index, symbol in enumerate(my_str):
 #     print(index, symbol)
-
-######################################################
-
-# str methods
-
+#
+#
+# ####################################################
+# Еще один пример - вложенные циклы (цикл в цикле).
+#
+# my_string_1 = "12"
+# my_string_2 = "34"
+# for symb_1 in my_string_1:
+# 	for symb_2 in my_string_2:
+# 		print(symb_1 + symb_2)
+#
+#
+#
+# Результат:
+# "13"	# перебирается все элементы из my_string_2 для элемента "1" из my_string_1
+# "14"
+# "23"	# перебирается все элементы из my_string_2 для элемента "2" из my_string_1
+# "24"
+#
+# ######################################################
+#
+# # str methods
+#
 # my_str = "qwerty"
-
+#
 # my_str_len = len(my_str)
 # print(my_str_len)
-
+#
 # print(my_str.isalpha())
-
-
+#
+#
 # my_str = "My name is John i'm 40"
-
+#
 # for symbol in my_str:
 #     if not symbol.isupper():
 #         print(symbol)
-
+#
 # for symbol in my_str:
 #     if symbol.islower() and symbol.isalpha() and symbol not in "eyuioa":
 #         print(symbol)
-
+#
 # result = my_str.find("John")
 # print(result)
-
-
+#
+#
 # filename = "images_123.png"
 # filename = filename.replace('png', 'jpg')
 # print(filename)
-
-
-
+#
+#
+#
 # my_str = "My name is Anna, i'm 40. It is dog"
 #
 # for symbol in my_str:
 #     if symbol.lower() in 'eyuioa':
 #         print(symbol)
-
-
+#
+#
 # my_str = "My name is Anna, i'm 40. It is dog"
+#
 #
 # print(my_str.lower().count('a'))
-
-
-# my_str = "My name is Anna, i'm 40. It is dog"
 #
+#
+# my_str = "My name is Anna, i'm 40. It is dog"
+
 # result = ''
 # for symbol in my_str:
 #     if symbol.isalpha() and symbol.islower() and symbol not in "eyuioa":
 #         result += symbol
 # print(result)
-
+#
 # filename = "images_png_123.png"
 # f_name = filename[::-1].replace('gnp', 'gpj', 1)
 # filename = f_name[::-1]
 # print(filename)
-
+#
 
 
 
