365795e04ca0374631366adb5e91d78b4ab7daf7
david <david.taranov@gmail.com>
2022-04-17 18:27:49 +0200

    add Homework_Lesson_9.py.py

--- Homework_Lesson_9.py
+++ Homework_Lesson_9.py
@@ -81,17 +81,17 @@ def crtList_individualSymbols(initial_str_1, initial_str_2):
 
 
 # 8
+import string, random
+from random import randint, choice
+
 surnames = ["Shevchenko", "Vasilenko", "Ovcharenko", "Borisenko"]
 domains = ["eu", "org", "best", "beer"]
 
 
 def generation_email():
-    import string, random
-    from random import randint, choice
     name = choice(surnames)
     domain = choice(domains)
     random_number = randint(100, 999)
     rand_string = ''.join(random.choice(string.ascii_lowercase) for _ in range(randint(5, 7)))
     e_mail = (f'{name}.{random_number}@{rand_string}.{domain}').lower()
     return e_mail
-
