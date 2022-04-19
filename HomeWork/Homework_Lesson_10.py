839545354901383f2e3b14893a3a932fe64f813d
david <david.taranov@gmail.com>
2022-04-19 18:19:37 +0200

    Homework_Lesson_10.py code changes

--- HomeWork/Homework_Lesson_10.py
+++ HomeWork/Homework_Lesson_10.py
@@ -31,16 +31,15 @@ result_2 = return_surnames('names.txt')
 
 # 3
 
-def return_calendar_dates_and_events(filename: str) -> list:
+def return_calendar_dates(filename: str) -> list:
     with open(filename, 'r') as my_file:
         data = my_file.read()
-        calendar_dates_and_events = []
+        calendar_dates = []
         for line in (data.split('\n')):
             if '-' in line:
                 calendar_date = line.split('-')[0]
-                event = line.split('-', 1)[1]
-                calendar_dates_and_events.append({calendar_date: event})
-        return calendar_dates_and_events
+                calendar_dates.append({"date": calendar_date})
+        return calendar_dates
 
 
-result_3 = return_calendar_dates_and_events('authors.txt')
+result_3 = return_calendar_dates('authors.txt')
